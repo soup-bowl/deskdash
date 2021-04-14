34c380d22eaff7fc5be96bdaa0ec1cf349f5ef16
soup-bowl <code@soupbowl.io>
2021-04-14 19:09:50 +0100

    Chart.

--- src/dashboard/dash.js
+++ src/dashboard/dash.js
@@ -1,5 +1,5 @@
 var endpoints = { 70: {'id': 70, 'endpoint': 'http://localhost:43594/'} };
-var data      = { 70: {'series': [ [], [], [] ]} };
+var data      = { 70: {'series': [ [], [] ]} };
 
 function init() {
 	for (let key in endpoints) {
@@ -13,6 +13,16 @@ function init() {
 				syst = json['content']['system'];
 				document.getElementById(obj.id + 'machine').innerHTML = syst['hostname'];
 				document.getElementById(obj.id + 'spec').innerHTML = syst['operating_system'] + ' ■ ' + syst['release'] + ' ■ ' + syst['processor'];
+
+				new Chartist.Line('.a'+ obj.id + 'chart', {
+					labels: [],
+					series: [ [], [] ]
+				}, {
+					fullWidth: true,
+					chartPadding: {
+						right: 40
+					}
+				});
 
 				segment.style.display = null;
 			})
@@ -36,6 +46,9 @@ function update() {
 
 				data[key].series[0].push(cpu); if ( data[key].series[0].length > 10 ) { data[key].series[0].shift(); }
 				data[key].series[1].push(mem); if ( data[key].series[1].length > 10 ) { data[key].series[1].shift(); }
+
+				chart = document.getElementsByClassName('a' + key + 'chart')[0];
+				chart.__chartist__.update({'series': data[key].series});
 			})
 			.catch(err => console.log(err));
 	}
