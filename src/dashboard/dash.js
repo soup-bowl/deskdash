ff122ddafc3c5ec74e9affa21c0b6d5b2c98f4a6
soup-bowl <code@soupbowl.io>
2021-04-15 16:33:46 +0100

    Removed ID need.

--- src/dashboard/dash.js
+++ src/dashboard/dash.js
@@ -1,19 +1,19 @@
 var endpoints;
-var data      = { 70: {'series': [ [], [], [] ]} };
+var data      = { 0: {'series': [ [], [], [] ]} };
 
 function init() {
 	document.getElementById('loading').style.display = null;
 	document.getElementById('stage').innerHTML = '';
-	for (let key in endpoints) {
-		var obj = endpoints[key];
+	for (let index = 0; index < endpoints.length; index++) {
+		var obj = endpoints[index];
 		fetch(obj.endpoint)
 			.then(response => response.json())
 			.then(json => {
 				file_get_contents("segments/communicator.html").then(page => {
-					rechanged = page.replaceAll('{{CHANGE}}', key);
+					rechanged = page.replaceAll('{{CHANGE}}', index);
 					document.getElementById("stage").innerHTML = rechanged;
 
-					load_new(obj, json);
+					load_new(index, json);
 				});
 			})
 			.catch(err => console.log(err));
@@ -21,20 +21,19 @@ function init() {
 	document.getElementById('loading').style.display = 'none';
 }
 
-function load_new(obj, json) {
-	var segment = document.getElementById(obj.id);
-	//data[key]['series'] = [ [], [] ];
+function load_new(id, json) {
+	var segment = document.getElementById(id);
 	
 	syst = json['content']['system'];
-	document.getElementById(obj.id + 'machine').innerHTML = syst['hostname'];
-	document.getElementById(obj.id + 'spec').innerHTML = syst['operating_system'] + ' ■ ' + syst['release'] + ' ■ ' + syst['processor'];
+	document.getElementById(id + 'machine').innerHTML = syst['hostname'];
+	document.getElementById(id + 'spec').innerHTML = syst['operating_system'] + ' ■ ' + syst['release'] + ' ■ ' + syst['processor'];
 
 	if (json['content']['gpu']['available']) {
-		var gpumon = document.getElementById(obj.id + 'graphicSegment');
+		var gpumon = document.getElementById(id + 'graphicSegment');
 		gpumon.style.display = null;
 	}
 
-	new Chartist.Line('.a'+ obj.id + 'chart', {
+	new Chartist.Line('.a'+ id + 'chart', {
 		labels: [],
 		series: [ [], [], [] ],
 	}, {
@@ -53,8 +52,8 @@ function load_new(obj, json) {
 }
 
 function update() {
-	for (let key in endpoints) {
-		var obj = endpoints[key];
+	for (let index = 0; index < endpoints.length; index++) {
+		var obj = endpoints[index];
 		fetch(obj.endpoint)
 			.then(response => response.json())
 			.then(json => {
@@ -62,32 +61,32 @@ function update() {
 
 				if (stat['cpu']['available']) {
 					cpu = stat['cpu'];
-					document.getElementById(obj.id + 'processorUsage').innerHTML = 'Usage: ' + cpu['cpu_usage']  + '%';
+					document.getElementById(index + 'processorUsage').innerHTML = 'Usage: ' + cpu['cpu_usage']  + '%';
 
-					data[key].series[0].push(cpu['cpu_usage']); if ( data[key].series[0].length > 10 ) { data[key].series[0].shift(); }
+					data[index].series[0].push(cpu['cpu_usage']); if ( data[index].series[0].length > 10 ) { data[index].series[0].shift(); }
 				}
 
 				if (stat['ram']['available']) {
 					mem = stat['ram'];
-					document.getElementById(obj.id + 'memoryUsage').innerHTML = 'Usage: ' + mem['real_memory_usage'] + '%';
-					document.getElementById(obj.id + 'swapUsage').innerHTML = 'Swap: ' + mem['swap_memory_usage'] + '%';
+					document.getElementById(index + 'memoryUsage').innerHTML = 'Usage: ' + mem['real_memory_usage'] + '%';
+					document.getElementById(index + 'swapUsage').innerHTML = 'Swap: ' + mem['swap_memory_usage'] + '%';
 
-					data[key].series[1].push(mem['real_memory_usage']); if ( data[key].series[1].length > 10 ) { data[key].series[1].shift(); }
+					data[index].series[1].push(mem['real_memory_usage']); if ( data[index].series[1].length > 10 ) { data[index].series[1].shift(); }
 				}
 
 				if (stat['gpu']['available']) {
 					gpu = stat['gpu'];
-					document.getElementById(obj.id + 'graphicUsage').innerHTML = 'Usage: ' + gpu['gpu_usage'] + '%';
+					document.getElementById(index + 'graphicUsage').innerHTML = 'Usage: ' + gpu['gpu_usage'] + '%';
 
-					data[key].series[2].push(gpu['gpu_usage']); if ( data[key].series[2].length > 10 ) { data[key].series[2].shift(); }
+					data[index].series[2].push(gpu['gpu_usage']); if ( data[index].series[2].length > 10 ) { data[index].series[2].shift(); }
 
-					gputmp = document.getElementById(obj.id + 'graphicTemp');
+					gputmp = document.getElementById(index + 'graphicTemp');
 					gputmp.innerHTML = 'Temp ' + gpu['gpu_temp_now'] + '°C';
 					set_temp_badge(gputmp, gpu['gpu_temp_now']);
 				}
 
-				chart = document.getElementsByClassName('a' + key + 'chart')[0];
-				chart.__chartist__.update({'series': data[key].series});
+				chart = document.getElementsByClassName('a' + index + 'chart')[0];
+				chart.__chartist__.update({'series': data[index].series});
 			})
 			.catch(err => console.log(err));
 	}
