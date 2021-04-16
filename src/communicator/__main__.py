41c72dd86a720cc307ac77437ca42e0c74c4b191
soup-bowl <code@soupbowl.io>
2021-04-16 19:49:47 +0100

    Optional authentication.

--- src/communicator/__main__.py
+++ src/communicator/__main__.py
@@ -1,19 +1,43 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 from communicator.stats import Stats
 import json
 import time
+import sys, os
 
 hostname = "0.0.0.0"
 port     = 43594
 
 class Server(BaseHTTPRequestHandler):
 	def do_GET(self):
-		self.set_headers(200, {'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'})
-		response = {
-			'success': True,
-			'content': Stats().get()
-		}
-		self.wfile.write(bytes(json.dumps(response), "utf-8"))
+		input_key = parse_qs( urlparse(self.path).query).get('key', None)
+
+		currentdir = os.path.split(os.path.abspath(__file__))[0]
+		use_key = False
+		key     = ''
+		try:
+			with open(currentdir + "/config.json") as json_file:
+				data = json.load(json_file)
+				use_key = data['auth']
+				key     = data['key']
+
+		except FileNotFoundError:
+			pass
+
+		if use_key == False or ( use_key == True and ( input_key != None and input_key[0] == key ) ):
+			self.set_headers(200, {'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+			response = {
+				'success': True,
+				'content': Stats().get()
+			}
+			self.wfile.write(bytes(json.dumps(response), "utf-8"))
+		else:
+			self.set_headers(401, {'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'})
+			response = {
+				'success': False,
+				'message': 'Either no key was provided, or it was incorrect.'
+			}
+			self.wfile.write(bytes(json.dumps(response), "utf-8"))
 	
 	def set_headers(self, response_code, headers):
 		self.send_response(response_code)
