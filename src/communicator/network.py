3f9c7c0c0cfad2c86c4da66fcc5d8a353ebdd3e7
soup-bowl <code@soupbowl.io>
2021-04-19 16:07:49 +0100

    Cache requests for 1 min.

--- src/communicator/network.py
+++ src/communicator/network.py
@@ -1,8 +1,28 @@
 import nmap
 import socket
+import json
+import os
+import time
 
 class Network(object):
 	def get_all(self):
+		conf  = 'netmem.json'
+
+		if os.path.isfile(conf):
+			if time.time() > (os.path.getmtime(conf) + 300 ):
+				os.remove(conf)
+			else:
+				with open(conf) as f:
+					hosts = json.load(f)
+					return hosts
+
+		hosts = self.net_scan_all()
+		with open(conf, 'w') as f:
+			json.dump(hosts, f)
+
+		return hosts
+
+	def net_scan_all(self):
 		hosts = {}
 		nm = nmap.PortScanner()
 		nm.scan(hosts='192.168.1.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
@@ -13,7 +33,7 @@ class Network(object):
 			a['hostname'] = self.get_hostname(host)
 			a['status']   = status
 			hosts[host.split(".")[3]] = a
-
+		
 		return hosts
 	
 	def get_hostname(self, ip):
