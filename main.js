fafbd0c183f187c2272c1414f961b3af0de6f7de
soup-bowl <code@soupbowl.io>
2021-05-14 17:23:52 +0100

    Hide menu bar if windowed.

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const fs = require('fs');
 
 function createWindow () {
 	const win = new BrowserWindow({
-		fullscreen: true
-	})
+		fullscreen: true,
+		autoHideMenuBar: true
+	});
 
-	win.loadFile('index.html')
+	win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-	createWindow()
+	createWindow();
 
 	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
-			createWindow()
+			createWindow();
 		}
 	})
-})
+});
 
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
-		app.quit()
+		app.quit();
 	}
-})
+});
