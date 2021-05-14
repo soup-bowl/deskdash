e932964899301aa7f69c8983adeb493ece7ddf10
soup-bowl <code@soupbowl.io>
2021-05-14 15:39:51 +0100

    Replaced Chromium kiosk reliance with Electron.

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+	const win = new BrowserWindow({
+		fullscreen: true
+	})
+
+	win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+	createWindow()
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
+})
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
 npm start &
-chromium-browser --kiosk -tab "http://localhost:9000"
